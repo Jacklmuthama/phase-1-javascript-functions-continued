532a9d4d30d24fa65218e3234d3b0622ef5b8480
Jackline <jackline.muthama@student.moringaschool.com>
2022-11-23 14:36:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(act="roller-skate"){
+    return `This Saturday, I want to ${act}!`
+}
+function mondayWork(act="go to the office"){
+    return `This Monday, I will ${act}.`
+}
+function wrapAdjective(wraps) {
+    if (wraps==="*") {
+        return (function (){
+            return `You are ${wraps}a hard worker${wraps}!`
+        })
+    }
+    else if (wraps==="||") {
+        return (function (){
+            return `You are ${wraps}a dedicated programmer${wraps}!`
+        })
+
+    }
+}   
+
+
